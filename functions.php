25a90536c3aa85fac4cccf446349f36cb1a1a7ac
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2014-05-16 16:10:30 +0100

    Friday 16th May 2014 12:40 - Remove plugin stylesheets.

--- functions.php
+++ functions.php
@@ -3,11 +3,13 @@ if ( !function_exists(core_mods) ) {
 	function core_mods() {
 		if ( !is_admin() ) {
 			wp_register_style( 'styles', get_stylesheet_uri(), null, filemtime( get_stylesheet_directory().'/style.css' ) );
+			wp_register_style( 'font-awesome', "//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css", null );
 			wp_register_script( 'slim-scroll', get_stylesheet_directory_uri() . '/_/js/jquery.slimscroll.min.js', array('jquery'), '1.0.0', true );
 			wp_register_script( 'bootstrap-tabs', get_stylesheet_directory_uri() . '/_/js/bootstrap-tabs.js', array('jquery','jquery-ui-core'), '1.0.0', true );
 			wp_register_script( 'functions', get_stylesheet_directory_uri() . '/_/js/functions.js', array('jquery', 'jquery-ui-core', 'bootstrap-all-min', 'slim-scroll', 'bootstrap-tabs'), '1.0.1', true );
 			//wp_register_script( 'img-fit', get_stylesheet_directory_uri() . '/_/js/jquery.imagefit.js', array('jquery'), '1.0.0', true );
 			
+			wp_enqueue_style('font-awesome');
 			wp_enqueue_style('styles');
 			wp_enqueue_script('slim-scroll');
 			wp_enqueue_script('bootstrap-tabs');
@@ -16,6 +18,18 @@ if ( !function_exists(core_mods) ) {
 		}
 	}
 	core_mods();
+}
+
+add_action( 'wp_print_styles', 'my_deregister_styles', 100 );
+
+function my_deregister_styles() {
+		wp_deregister_style( 'font-awesome-four' );
+		wp_deregister_style( 'theme-my-login' );
+		wp_deregister_style( 'abc_style' );
+		wp_deregister_style( 'cookielawinfo-style' );
+		wp_deregister_style( 'wprssmi_template_styles' );
+		wp_deregister_style( 'fancybox' );
+       // deregister as many stylesheets as you need...
 }
 
 add_theme_support('html5', array('search-form'));
