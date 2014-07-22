5feeac608ab5f059ac7b2b52ad12f95b94b64eee
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2014-07-22 12:49:03 +0100

    Tuesday 22 July 2014 - Add location note just for ultimate package.

--- _/inc/packages/single-package-options.php
+++ _/inc/packages/single-package-options.php
@@ -19,7 +19,7 @@
 			
 			</ul>
 			
-			<?php if ($post->post_name == "motopro-executive" || $post->post_name == "motopro-ultimate") { ?>
+			<?php if ($post->post_name == "motopro-ultimate") { ?>
 			<small>*Meeting location requests may be declined if it is felt the location may pose a risk to the health and safety of the MP employee or agent.</small>
 			<?php } ?>
 			
